180cfbf89d8abf5a56249e400e9ecbea19fe9e64
Nano <Nano@users.noreply.github.com>
2020-11-07 00:03:07 +0100

    main + test changes

--- API/main.py
+++ API/main.py
@@ -1,8 +1,25 @@
 #Run the server
+from flask import Flask, request
+app = Flask(__name__)
 
+@app.route('/hello_world', methods=['GET'])
+def hello():
+    print('Hello, World!')
+    return 'Hello, World!'
 
-
+@app.route('/user', methods=['POST','GET'])
+def get_username():
+    if request.method == 'POST':
+        print('POST')
+        username = request.form['username']
+        print(username)
+    if request.method == 'GET':
+        print('GET')
+        username = request.form['username']
+        print(username)
+    return username
 
 
 if __name__ == "__main__":
+    app.run(debug=True)
     pass
--- API/test.py
+++ API/test.py
@@ -1,12 +1,20 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/hello_world', methods=['GET'])
-def hello():
-    print('Hello, World!')
-    return 'Hello, World!'
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      print('POST', user)
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      print('GET', user)
+      return redirect(url_for('success',name = user))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug = True)
