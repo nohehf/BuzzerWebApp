9732eb8482f8d81f50abe40b2c3610b7b83db708
nohehf <nohe.hinniger.foray@gmail.com>
2020-11-06 21:40:38 +0100

    Ajout de la fonction principale

--- Web/buzz.js
+++ Web/buzz.js
@@ -1,3 +1,10 @@
+
+// Called by clicking the buzz button
+function onClick(){
+    msg();
+}
+
+// a simple test function
 function msg() {
     var playerName = document.getElementById("name").value;
     alert("You just buzzed as: "+ playerName);
