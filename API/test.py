577ad8652a9264c6d7339144c9968228ec14af51
Nano <Nano@users.noreply.github.com>
2020-11-07 00:04:30 +0100

    Update test.py

--- API/test.py
+++ API/test.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 def success(name):
    return 'welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/user',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
+      user = request.form['username']
       print('POST', user)
       return redirect(url_for('success',name = user))
    else:
-      user = request.args.get('nm')
+      user = request.args.get('username')
       print('GET', user)
       return redirect(url_for('success',name = user))
 
