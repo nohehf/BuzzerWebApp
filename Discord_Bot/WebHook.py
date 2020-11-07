63f950fd8a363c61789f42ab9b41b1ca83566b3f
Nano <Nano@users.noreply.github.com>
2020-11-07 22:36:14 +0100

    Update WebHook.py

--- Discord_Bot/WebHook.py
+++ Discord_Bot/WebHook.py
@@ -1,17 +1,29 @@
 import requests
 import discord
+import os
+import sys
 from discord import Webhook, RequestsWebhookAdapter, File
 
 def get_webhook_info(): #On récupère les info pour utiliser le webhook
-    txt = open('url.txt','r')
+    current_path = os.getcwd()  # On setup le bon path pour être sur d'avoir les modules de bien importés, peut n'importe l'interpréteur.
+    split = current_path.split('\\')
+    if split[-1] != 'Discord_Bot':
+        split.pop()
+        split.append('Discord_Bot')
+        BuzzerWebApp_folder_path = '\\'.join(split)
+        sys.path.append(BuzzerWebApp_folder_path)
+    else:
+        BuzzerWebApp_folder_path = current_path
+    txt_path = BuzzerWebApp_folder_path + r'\url.txt'
+    txt = open(txt_path,'r')
     WEBHOOK_ID = txt.readline()
     WEBHOOK_TOKEN = txt.readline()
     url = txt.readline()
     txt.close()
     return (WEBHOOK_ID,WEBHOOK_TOKEN)
 
-def create_webhook_object(discord_folder_path):# On crée l'objet webhook pour lui faire faire des actions
-    (WEBHOOK_ID,WEBHOOK_TOKEN) = get_webhook_info(discord_folder_path)
+def create_webhook_object():# On crée l'objet webhook pour lui faire faire des actions
+    (WEBHOOK_ID,WEBHOOK_TOKEN) = get_webhook_info()
     webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter())
     return webhook
 
